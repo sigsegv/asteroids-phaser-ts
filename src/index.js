ff6ee84176ed207c16c48c87645e9e1b7f60e4c5
sigsegv <sigsegv@users.noreply.github.com>
2019-01-17 21:08:40 -0800

    minimal npm and webpack setup.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function component() {
+    let element = document.createElement('div');
+  
+    element.innerHTML = 'asteroids-phaser-ts';
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: false,
+    port: 8080
+  }
+};
